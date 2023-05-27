a8bc259c3efe5a25bdb5b648a47501c2cc793885
jushaan <jushaan18@gmail.com>
2023-05-27 19:15:12 +0530

    completed workflow for embeddings

--- jobxmlc/make_initial_embeddings/data_filter.py
+++ jobxmlc/make_initial_embeddings/data_filter.py
@@ -1,14 +1,41 @@
 from jobxmlc.registry import ENCODER, DATA_FILTER, register
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from tqdm import tqdm
+
 
 @register(_name="tf-idf", _type=DATA_FILTER)
 class tfidf10:
-    def __init__(self):
-        pass
-    def preprocessing_data(self):
-        return NotImplementedError
-    def tokenizer(self):
-        return NotImplementedError
-    def create_embeddings(self):
-        return NotImplementedError
-    def save_embeddings(self):
-        return NotImplementedError
+    def __init__(self,args):
+        self.args = args
+    def word_tokenizer(self, text):
+        """
+        tokenize a sentence into words
+        """
+        return text.split() 
+    def preprocessing_data(self,corpus):
+        """
+        remove the 10 most unimportant words in a job based on tf-idf scores
+        """
+        vectorizer = TfidfVectorizer(tokenizer=self.word_tokenizer)
+        X = vectorizer.fit_transform(corpus)
+        Y = vectorizer.get_feature_names()
+        full_corpus_new = []
+        for i in tqdm(range(len(corpus))):
+            dicti = dict(zip(vectorizer.get_feature_names(), X.toarray()[i]))
+            lisi=[]
+            for text in corpus[i].split():
+                lisi.append(dicti[text])
+            lisi_index = sorted(range(len(lisi)), key=lambda k: lisi[k])
+            lisi_index=lisi_index[:self.args.number_of_words]
+            corpus_split = corpus[i].split()
+            corpus_split_pre = []
+
+            for i, item in enumerate(corpus_split):
+                if i in lisi_index:
+                    continue
+                else:
+                    corpus_split_pre.append(item)
+            corpus_preprocessed = " ".join(corpus_split_pre)
+            full_corpus_new.append(corpus_preprocessed)
+        return full_corpus_new
--- jobxmlc/make_initial_embeddings/encoder.py
+++ jobxmlc/make_initial_embeddings/encoder.py
@@ -2,8 +2,7 @@ from sentence_transformers import SentenceTransformer
 import argparse
 from tqdm import tqdm
 import numpy as np
-import pickle
-from sklearn.feature_extraction.text import TfidfVectorizer
+
 import os
 from jobxmlc.registry import ENCODER, DATA_FILTER, register
 class initialEmbeddings:
@@ -15,44 +14,33 @@ class initialEmbeddings:
         return NotImplementedError
     def create_embeddings(self):
         return NotImplementedError
-    def save_embeddings(self):
-        return NotImplementedError
+    def save_embeddings(self,data_name, data_embeddings):
+        print(f"Saving {data_name} embeddings to file...")
+        save_path = args.embeddings_save_dir
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+        np.save(os.path.join(save_path, f"{data_name}_embeddings.npy"), data_embeddings)
+    def embeddings_runner(self):
+        """
+        run the embeddings pipeline for the dataset"""
+        data_directory_path = self.args.dataset_path
+        train_data_path = os.path.join(data_directory_path,'trn_X.txt')
+        test_data_path = os.path.join(data_directory_path,'tst_X.txt')
+        labels_path = os.path.join(data_directory_path,'Y.txt')
+
+        for data_name, data in zip(['train','test','label'],[train_data_path, test_data_path, labels_path]):
+            if not os.path.exists(data):
+                raise FileNotFoundError("File {} not found".format(data))
+            data_embeddings = self.create_embeddings(data)
+            self.save_embeddings(self,data_name, data_embeddings) 
 
 @register(_name="sentence-encoder", _type=ENCODER)
 class SentenceTransformerEmbeddings(initialEmbeddings):
     def __init__(self,args):
         self.args = args
-    def word_tokenizer(self, text):
-        """
-        tokenize a sentence into words
-        """
-        return text.split() 
     def preprocessing_data(self,corpus):
-        """
-        remove the 10 most unimportant words in a job based on tf-idf scores
-        """
-        vectorizer = TfidfVectorizer(tokenizer=self.word_tokenizer)
-        X = vectorizer.fit_transform(corpus)
-        Y = vectorizer.get_feature_names()
-        full_corpus_new = []
-        for i in tqdm(range(len(corpus))):
-            dicti = dict(zip(vectorizer.get_feature_names(), X.toarray()[i]))
-            lisi=[]
-            for text in corpus[i].split():
-                lisi.append(dicti[text])
-            lisi_index = sorted(range(len(lisi)), key=lambda k: lisi[k])
-            lisi_index=lisi_index[:10]
-            corpus_split = corpus[i].split()
-            corpus_split_pre = []
-
-            for i, item in enumerate(corpus_split):
-                if i in lisi_index:
-                    continue
-                else:
-                    corpus_split_pre.append(item)
-            corpus_preprocessed = " ".join(corpus_split_pre)
-            full_corpus_new.append(corpus_preprocessed)
-        return full_corpus_new
+        data_filter = DATA_FILTER[self.args.data_filter.name](self.args.data_filter.params)
+        data_filter.preprocessing_data(corpus)
     def create_embeddings(self):
         model_name = self.args.model
         embedder = SentenceTransformer(model_name)
@@ -63,30 +51,11 @@ class SentenceTransformerEmbeddings(initialEmbeddings):
         print("Encoding {} sentences using {}.....\n".format(len(dataset), args.model))
         embedding_size = embedder.get_sentence_embedding_dimension()
         corpus_embeddings = np.empty((0, embedding_size)) 
-        dataset = self.preprocess(dataset)
+        dataset = self.preprocessing_data(dataset)
         for batch in tqdm(np.array_split(dataset, self.batch_size)):
             embs = embedder.encode(batch)
             corpus_embeddings = np.vstack((corpus_embeddings, embs))
         return corpus_embeddings
-    
-    def save_embeddings(self):
-        print("Saving embeddings to file...")
-        save_path = args.save_path
-
-        np.save("./../dumps/BERTMEAN-{}.npy".format(args.output), corpus_embeddings)
-    
-    def embeddings_runner(self):
-        """
-        run the embeddings pipeline for the dataset"""
-        data_directory_path = self.args.dataset_path
-        train_data_path = os.path.join(data_directory_path,'trn_X.txt')
-        test_data_path = os.path.join(data_directory_path,'tst_X.txt')
-        labels_path = os.path.join(data_directory_path,'Y.txt')
-
-        for data in [train_data_path, test_data_path, labels_path]:
-            if not os.path.exists(data):
-                raise FileNotFoundError("File {} not found".format(data))
-            data_embeddings = self.create_embeddings(data)   
 
 
 
