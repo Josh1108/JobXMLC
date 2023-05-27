5d8dffd2c8a8ff5749819a1b78f7101c604c5cb0
jushaan <jushaan18@gmail.com>
2023-05-27 19:38:29 +0530

    inserted fasttext embeddings

--- jobxmlc/make_initial_embeddings/encoder.py
+++ jobxmlc/make_initial_embeddings/encoder.py
@@ -2,24 +2,33 @@ from sentence_transformers import SentenceTransformer
 import argparse
 from tqdm import tqdm
 import numpy as np
-
+import fasttext
 import os
 from jobxmlc.registry import ENCODER, DATA_FILTER, register
 class initialEmbeddings:
     def __init__(self):
         pass
-    def preprocessing_data(self):
-        return NotImplementedError
     def tokenizer(self):
         return NotImplementedError
     def create_embeddings(self):
         return NotImplementedError
+    def preprocessing_data(self,corpus):
+        data_filter = DATA_FILTER[self.args.data_filter.name](self.args.data_filter.params)
+        data_filter.preprocessing_data(corpus)
     def save_embeddings(self,data_name, data_embeddings):
         print(f"Saving {data_name} embeddings to file...")
         save_path = args.embeddings_save_dir
         if not os.path.exists(save_path):
             os.makedirs(save_path)
         np.save(os.path.join(save_path, f"{data_name}_embeddings.npy"), data_embeddings)
+    
+    def load_file(self,data_path):
+        f = open(data_path,'r').readlines()
+        dataset=[x.split('\n')[0] for x in f]
+        print("First row of dataset:", dataset[0])
+        print("Encoding {} sentences using {}.....\n".format(len(dataset), args.model))
+        return dataset
+    
     def embeddings_runner(self):
         """
         run the embeddings pipeline for the dataset"""
@@ -28,37 +37,47 @@ class initialEmbeddings:
         test_data_path = os.path.join(data_directory_path,'tst_X.txt')
         labels_path = os.path.join(data_directory_path,'Y.txt')
 
-        for data_name, data in zip(['train','test','label'],[train_data_path, test_data_path, labels_path]):
+        for data_name, data_path in zip(['train','test','label'],[train_data_path, test_data_path, labels_path]):
             if not os.path.exists(data):
                 raise FileNotFoundError("File {} not found".format(data))
+            data = self.load_file(data_path)
             data_embeddings = self.create_embeddings(data)
             self.save_embeddings(self,data_name, data_embeddings) 
 
-@register(_name="sentence-encoder", _type=ENCODER)
-class SentenceTransformerEmbeddings(initialEmbeddings):
+@register(_name="sentence-transformer-encoder", _type=ENCODER)
+class SentenceTransformerEncoder(initialEmbeddings):
     def __init__(self,args):
         self.args = args
-    def preprocessing_data(self,corpus):
-        data_filter = DATA_FILTER[self.args.data_filter.name](self.args.data_filter.params)
-        data_filter.preprocessing_data(corpus)
-    def create_embeddings(self):
+    def create_embeddings(self,data):
         model_name = self.args.model
         embedder = SentenceTransformer(model_name)
-        dataset_path = self.args.dataset_path
-        f = open(dataset_path,'r').readlines()
-        dataset=[x.split('\n')[0] for x in f]
-        print("First row of dataset:", dataset[0])
-        print("Encoding {} sentences using {}.....\n".format(len(dataset), args.model))
         embedding_size = embedder.get_sentence_embedding_dimension()
         corpus_embeddings = np.empty((0, embedding_size)) 
-        dataset = self.preprocessing_data(dataset)
+        dataset = self.preprocessing_data(data)
         for batch in tqdm(np.array_split(dataset, self.batch_size)):
             embs = embedder.encode(batch)
             corpus_embeddings = np.vstack((corpus_embeddings, embs))
         return corpus_embeddings
 
+@register(_name='fasttext', _type=ENCODER)
+class FasttextEncoder(initialEmbeddings):
+    def __init__(self,args):
+        self.args = args
+    def create_embeddings(self,data):
+        modelpath = self.args.model_path
+        model = fasttext.load_model(modelpath)
+        embedding_size = model.get_dimension()
+        corpus_embeddings = np.empty((0, embedding_size)) 
+        dataset = self.preprocessing_data(data)
+        vectors =[]
+        for i in range(len(dataset)):
+            vectorlist=[model[word] for word in dataset.split()]
+            jobvector=np.mean(vectorlist, axis=0)
+            corpus_embeddings = np.vstack((corpus_embeddings, jobvector))
+        return vectors
+        
 
-
+    
 
 
 if __name__ == '__main__':
